2c62c9b72687ee8da4f15639a95fab8a042f30fd
majorBien <shadow7.dev@gmail.com>
2024-10-05 17:36:40 +0200

    lora receive works

--- main/main.c
+++ main/main.c
@@ -1,11 +1,37 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
 
-void app_main(void)
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "ra01s.h"
+#include "lora.h"
+#include "nvs_flash.h"
+
+
+static const char *TAG = "MAIN";
+
+
+
+void app_main()
 {
-    while (true) {
-        printf("Hello from app_main!\n");
-        sleep(1);
-    }
+	//openFirstAidKit = 0;
+	ESP_LOGI(TAG, "APP START");
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+	
+
+	loraInit();
+	loraTaskStart();
+
+
+
 }
